4c2b70cf8ffd8c48e072fb03cbb3b12b7f5719ac
John Lucas Rodrigues Aristóteles <xjl16@outlook.com>
2023-07-31 11:09:55 -0300

    Criação da Classe Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            //var valor = 10.25;
+            decimal valor = 10.25m; //OPÇÃO MAIS VIÁVEL
+
+            //CONVERSÃO PARA A NOTAÇÃO BRASILEIRA, UTILIZANDO VIRGULA PARA CASAS DECIMAIS
+            // UTILIZANDO C(CURRENT) COMO PARÂMETRO, O VALOR JÁ EXIBIDO EM MOEDA
+            Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("es-US"))); //Inglês
+            Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); //Português
+        }
+    }
+}
