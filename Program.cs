024882ef91d1c5038a0ff047781c9579dbe9ec5d
John Lucas Rodrigues Aristóteles <xjl16@outlook.com>
2023-07-31 12:18:37 -0300

    Utilizando C como parâmetro Currency e método Math

--- Program.cs
+++ Program.cs
@@ -12,9 +12,13 @@ namespace Moedas
             decimal valor = 10.25m; //OPÇÃO MAIS VIÁVEL
 
             //CONVERSÃO PARA A NOTAÇÃO BRASILEIRA, UTILIZANDO VIRGULA PARA CASAS DECIMAIS
-            //UTILIZANDO C(CURRENT) COMO PARÂMETRO, O VALOR JÁ EXIBIDO EM MOEDA
-            Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("es-US"))); //Inglês
-            Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); //Português
+            //UTILIZANDO C(CURRENCY) COMO PARÂMETRO, O VALOR JÁ EXIBIDO EM MOEDA
+            //Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("es-US"))); //Inglês
+            //Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); //Português
+            Console.WriteLine(Math.Round(valor)); //MÉTODO PARA ARRENDONDAR
+            Console.WriteLine(Math.Ceiling(valor)); //MÉTODO PARA ARRENDONDAR PARA CIMA
+            Console.WriteLine(Math.Floor(valor)); //MÉTODO PARA ARRENDONDAR PARA BAIXO
+
         }
     }
 }
