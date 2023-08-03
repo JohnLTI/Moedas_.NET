9438eba08e62b43bd73f46a89a229d6cb9a8fe55
John Lucas Rodrigues Aristóteles <xjl16@outlook.com>
2023-08-02 22:45:49 -0300

    Alterações e atualizações

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using System.Globalization;
 
-namespace Moedas
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Moedas {
+    class Program {
+        static void Main(string[] args) {
             Console.Clear();
             //var valor = 10.25;
             decimal valor = 10.25m; //OPÇÃO MAIS VIÁVEL
 
             //CONVERSÃO PARA A NOTAÇÃO BRASILEIRA, UTILIZANDO VIRGULA PARA CASAS DECIMAIS
             //UTILIZANDO C(CURRENCY) COMO PARÂMETRO, O VALOR JÁ EXIBIDO EM MOEDA
-            //Console.WriteLine(valor.ToString(CultureInfo.CreateSpecificCulture("es-US"))); //Inglês
-            //Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); //Português
+            Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("es-US"))); //Inglês
+            Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR"))); //Português
             Console.WriteLine(Math.Round(valor)); //MÉTODO PARA ARRENDONDAR
             Console.WriteLine(Math.Ceiling(valor)); //MÉTODO PARA ARRENDONDAR PARA CIMA
             Console.WriteLine(Math.Floor(valor)); //MÉTODO PARA ARRENDONDAR PARA BAIXO
-
         }
     }
 }
